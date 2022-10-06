99d5c823d56bde743779542e2be5dd8ee0bfb957
Pierre Åkerman <pirreakerman@gmail.com>
2022-10-06 11:23:17 +0200

    Setup resource dictionaries, components and viewmodel with view

---
+++ SmartApp/MVVM/ViewModels/KitchenViewModel.cs
@@ -0,0 +1,23 @@
+﻿using SmartApp.MVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartApp.MVVM.ViewModels
+{
+    internal class KitchenViewModel
+    {
+        private ObservableCollection<DeviceItem> _deviceItems;
+        public KitchenViewModel()
+        {
+            _deviceItems = new ObservableCollection<DeviceItem>();
+        }
+        public string Title { get; set; } = "Kitchen";
+        public string Temperature { get; set; } = "23 °C";
+        public string Humidity { get; set; } = "34 %";
+        public IEnumerable<DeviceItem> DeviceItems => _deviceItems;
+    }
+}
---
+++ SmartApp/MVVM/ViewModels/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using SmartApp.MVVM.Cores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartApp.MVVM.ViewModels
+{
+    internal class MainViewModel : ObservableObject
+    {
+        public RelayCommand KitchenViewCommand { get; set; }
+        public KitchenViewModel KitchenViewModel { get; set; }
+        private object _currentView;
+
+        public MainViewModel()
+        {
+            KitchenViewModel = new KitchenViewModel();
+            KitchenViewCommand = new RelayCommand(x => { CurrentView = KitchenViewModel; });
+        }
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
